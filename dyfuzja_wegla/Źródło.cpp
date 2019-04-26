1faf4017c0dfad32d15b23c2299ddf1df09c543d
actiasst <maciekactias@gmail.com>
2019-04-26 13:43:24 +0200

    Add files via upload

--- dyfuzja_wegla/Źródło.cpp
+++ dyfuzja_wegla/Źródło.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -13,51 +15,66 @@ int main() {
 	double d0 = 0.000041;
 	double D = d0 * (exp(-Q / (R * temp_kelwin))) * 1E10;
 	double t = 1;
+	double deltaTemp = 0;
 
-	double tab_wynik[ROZMIAR];
-	double tab_wynik_tmp[ROZMIAR];
-	for (int i = 0; i < ROZMIAR; i++) {
-		tab_wynik[i] = 0;
-		tab_wynik_tmp[i] = 0;
-	}
+	string nazwa = "";
+	fstream plik;
+	while (true) {
+		cout << "Podaj temperature: ";
+		cin >> temp;
+		cout << "Podaj nagrzewanie: ";
+		cin >> deltaTemp;
+		cout << "Podaj krok czasowy: ";
+		cin >> t;
+		deltaTemp *= t;
+		cout << "Podaj nazwe pliku: ";
+		cin.ignore();
+		getline(cin, nazwa);
 
-	for (int i = 0; i < 6; i++) {
-		tab_wynik[i] = 0.67;
-		tab_wynik_tmp[i] = 0.67;
-	}
 
-	for (int i = 6; i < ROZMIAR; i++) {
-		tab_wynik[i] = 0.02;
-		tab_wynik_tmp[i] = 0.02;
-	}
-
-	int obecna_komorka = 6;
-	//for (int i = 0; i < 0; i++) {
-	//	tab_wynik_tmp[0] = (1 - (2 * D * t))*tab_wynik[0] + D * t *(tab_wynik[1] + tab_wynik[1]);
-	//	for (int j = 1; j < ROZMIAR - 1; j++) {
-	//		tab_wynik_tmp[j] = (1 - (2 * D * t))*tab_wynik[j] + D * t *(tab_wynik[j - 1] + tab_wynik[j + 1]);
-	//	}
-	//	tab_wynik_tmp[99] = (1 - (2 * D * t))*tab_wynik[99] + D * t *(tab_wynik[98] + tab_wynik[98]);
-	//	for (int j = 0; j < ROZMIAR; j++)
-	//		tab_wynik[j] = tab_wynik_tmp[j];
-	//}
-	for (int i = 0; i < 44; i++) {
-		tab_wynik_tmp[0] = (1 - (2 * D * t))*tab_wynik[0] + D * t *(tab_wynik[1] + tab_wynik[1]);
-		for (int j = 1; j <= obecna_komorka; j++) {
-			tab_wynik_tmp[j] = (1 - (2 * D * t))*tab_wynik[j] + D * t *(tab_wynik[j - 1] + tab_wynik[j + 1]);
+		double tab_wynik[ROZMIAR];
+		double tab_wynik_tmp[ROZMIAR];
+		for (int i = 0; i < ROZMIAR; i++) {
+			tab_wynik[i] = 0;
+			tab_wynik_tmp[i] = 0;
 		}
-		tab_wynik_tmp[obecna_komorka] = (1 - (2 * D * t))*tab_wynik[obecna_komorka] + D * t *(tab_wynik[obecna_komorka-1] + tab_wynik[obecna_komorka-1]);
-		for (int j = 0; j < ROZMIAR; j++)
-			tab_wynik[j] = tab_wynik_tmp[j];
-		if (!(0 < (-245.5)*tab_wynik[obecna_komorka] + 912 - temp)) 
-			obecna_komorka++;
+
+		for (int i = 0; i < 6; i++) {
+			tab_wynik[i] = 0.67;
+			tab_wynik_tmp[i] = 0.67;
+		}
+
+		for (int i = 6; i < ROZMIAR; i++) {
+			tab_wynik[i] = 0.02;
+			tab_wynik_tmp[i] = 0.02;
+		}
+
+		int obecna_komorka = 6;
+
+		for (int i = 0; i < 200; i++) {
+			D = d0 * (exp(-Q / (R * temp_kelwin))) * 1E10;
+			temp_kelwin = temp + 273;
+			for (int j = 1; j < obecna_komorka; j++)
+				tab_wynik_tmp[j] = (1 - 2 * D * t)*tab_wynik[j] + D * t *(tab_wynik[j - 1] + tab_wynik[j + 1]);
+			tab_wynik_tmp[0] = (1 - 2 * D * t)*tab_wynik[0] + D * t *(tab_wynik[1] + tab_wynik[1]);
+			tab_wynik_tmp[obecna_komorka] = (1 - 2 * D * t)*tab_wynik[obecna_komorka] + D * t *(tab_wynik[obecna_komorka - 1] + tab_wynik[obecna_komorka - 1]);
+			for (int j = 0; j <= obecna_komorka; j++)
+				tab_wynik[j] = tab_wynik_tmp[j];
+			if (!(0 < (-245.5)*tab_wynik[obecna_komorka] + 912 - temp)) {
+				obecna_komorka++;
+				if (obecna_komorka > ROZMIAR)
+					obecna_komorka = ROZMIAR;
+			}
+			temp += deltaTemp;
+		}
+
+
+
+		plik.open("C:\\Users\\Lord Actias\\Downloads\\" + nazwa + ".csv", ios::out);
+		for (int i = 0; i < ROZMIAR; i++)
+			plik << tab_wynik[i] << endl;
+		plik.close();
 	}
-
-
-	for (int i = 0; i < 10; i++) {
-		cout << i << ": " << tab_wynik[i] << endl;
-	}
-
 
 	system("pause");
 }
